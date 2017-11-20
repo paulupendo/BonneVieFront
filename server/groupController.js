40fce5b626b852a42cfaf75bcc133d85c1dc6bed
candy_man <lovepaul46@outlook.com>
2017-11-20 16:42:57 +0300

    [chore]Implement FP

--- server/groupController.js
+++ server/groupController.js
@@ -1,15 +1,16 @@
 const R = require('ramda')
+const isEmptyString = R.pipe(
+  R.defaultTo(''),
+  R.trim,
+  R.isEmpty
+)
+
 module.exports = {
   NewGroup: function (name) {
     this.name = name
   },
   validator: function (name) {
     var response
-    const isEmptyString = R.pipe(
-      R.defaultTo(''),
-      R.trim,
-      R.isEmpty
-    )
     var emptyInput = R.any(isEmptyString, [name])
     if (emptyInput) {
       response = 'Ooops! Group name cannot be empty'
