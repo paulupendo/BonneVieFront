ef94c3c55a6e70add7e703264b66e4c5fd109908
candy_man <lovepaul46@outlook.com>
2017-11-20 16:41:41 +0300

    [feature]Auth endpoints v0.0.1

--- server/authController.js
+++ server/authController.js
@@ -1,5 +1,11 @@
 const R = require('ramda')
 
+const isEmptyString = R.pipe(/** function composition with Ramda */
+  R.defaultTo(''),
+  R.trim,
+  R.isEmpty
+) /** use ramda fp for input validation */
+
 module.exports = {
   UserInfo: function (name, email, password, confirmPass) { /** user object constructor */
     this.name = name
@@ -12,12 +18,9 @@ module.exports = {
     var regx = /^[a-z0-9]+@[a-z0-9]+(?:\.[a-z0-9])/ /**
      * email matching regExp
      */
-    const isEmptyString = R.pipe(
-      R.defaultTo(''),
-      R.trim,
-      R.isEmpty
-    ) /** use ramda fp for input validation */
-    var emptyField = R.any(isEmptyString, [name, email, password, confirmPassword])
+    var emptyField = R.any(isEmptyString, [name, email, password, confirmPassword]) /**
+     * returns boolean after cheking if input is empty
+     */
     if (emptyField) {
       message = 'Ooops! All fields are required for you to sign up'
     } else if (!regx.test(email)) {
@@ -31,20 +34,62 @@ module.exports = {
     }
     return message
   },
-  userAuth: function (username, password) { /** signin credentials validator */
-    return 'success'
+  userAuth: function (usersModel, userObj, res) { /** signin handler */
+    var emptyField = R.any(isEmptyString, [userObj.name, userObj.password])
+
+    if (emptyField) {
+      res.json({ msg: 'Ooops! All input fields are required' })
+    } else if (userObj.password.length <= 8) {
+      res.json({ msg: 'Ooops! Passwords must have more than 8 characters' })
+    } else {
+      usersModel.find((err, users) => { /** move this to own function during refactor */
+        if (err) {
+          console.log(err)
+          res.json({ msg: 'Ooops! An error occured' })
+        } else {
+          var userExists = users.some((user) => {
+            return user.username === R.trim(userObj.name) && user.password === R.trim(userObj.password)
+          }) /** return true if username and password found */
+          if (userExists) {
+            res.json({ msg: 'Successful logged in!' })
+          } else {
+            res.json({ msg: 'Ooops! username or password do not match' })
+          }
+        }
+      })
+    }
   },
-  save: function (users, name, email, password, res) { /** save data to MongoDB */
-    users.username = name
-    users.password = password
-    users.email = email
+  miniSave: function (users, name, email, password, res) { /** save data to MongoDB */
+    users.username = R.trim(name)
+    users.password = R.trim(password)
+    users.email = R.trim(email)
+
     const isError = (err) => {
       if (err) {
         res.json({msg: err.message})
       } else {
-        res.json({msg: 'Registration successful'})
+        res.json({msg: R.trim(name) + '! Hurrah! You have successfuly been registered.'}) // Data saved to DB
       }
     }
+
     users.save(isError) /** first class function. FP Hurrah!! */
+  },
+  save: function (usersModel, UserMInstance, userObj, res) {
+    usersModel.find((err, users) => { /** move this to own function during refactor */
+      if (err) {
+        console.log(err)
+      } else {
+        var userExists = users.some((user) => {
+          return user.username === R.trim(userObj.name)
+        }) /** return true if username found */
+        if (userExists) {
+          res.json({ msg: 'Ooops! This username is already taken' })
+        } else {
+          this.miniSave(UserMInstance, userObj.name, userObj.email, userObj.password, res) /**
+          * save users to mongoDB via MLab if all edge-cases pass
+          */
+        }
+      }
+    })
   }
 }
