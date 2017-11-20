db751bfd6e79ddab39791efc685e0f005b7e6c3a
candy_man <lovepaul46@outlook.com>
2017-11-20 16:40:58 +0300

    [chore]Unassign _id property from db SChemas

--- models/db_models.js
+++ models/db_models.js
@@ -4,26 +4,23 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var usersSchema = new Schema({
-  _id: Schema.Types.ObjectId,
   username: String,
   email: String,
-  password: String,
-  groups: [{ type: Schema.Types.ObjectId, ref: 'Groups' }],
-  messages: [{ type: Schema.Types.ObjectId, ref: 'Messages' }]
+  password: String
+  // groups: [{ type: Schema.Types.ObjectId, ref: 'Groups' }],
+  // messages: [{ type: Schema.Types.ObjectId, ref: 'Messages' }]
 })
 
 var groupSchema = new Schema({
-  _creator: { type: Schema.Types.ObjectId, ref: 'Users' },
-  messages: [{ type: Schema.Types.ObjectId, ref: 'Messages' }],
-  groupid: Schema.Types.ObjectId,
+  // _creator: { type: Schema.Types.ObjectId, ref: 'Users' },
+  // messages: [{ type: Schema.Types.ObjectId, ref: 'Messages' }],
   name: String,
   members: String
 })
 
 var messageSchema = new Schema({
-  _creator: { type: Schema.Types.ObjectId, ref: 'Users' },
-  _group: { type: Schema.Types.ObjectId, ref: 'Groups' },
-  message_id: Schema.Types.ObjectId,
+  // _creator: { type: Schema.Types.ObjectId, ref: 'Users' },
+  // _group: { type: Schema.Types.ObjectId, ref: 'Groups' },
   message: String
 })
 
