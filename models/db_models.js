8bbfa6702301e471c8e6864510d5e32320f4a297
candy_man <lovepaul46@outlook.com>
2017-11-24 12:42:46 +0300

    [chore]Update DB model to fit document referencing

--- models/db_models.js
+++ models/db_models.js
@@ -6,21 +6,21 @@ var Schema = mongoose.Schema
 var usersSchema = new Schema({
   username: String,
   email: String,
-  password: String
-  // groups: [{ type: Schema.Types.ObjectId, ref: 'Groups' }],
-  // messages: [{ type: Schema.Types.ObjectId, ref: 'Messages' }]
+  password: String,
+  groups: [{ type: Schema.Types.ObjectId, ref: 'Groups' }],
+  messages: [{ type: Schema.Types.ObjectId, ref: 'Messages' }]
 })
 
 var groupSchema = new Schema({
-  // _creator: { type: Schema.Types.ObjectId, ref: 'Users' },
-  // messages: [{ type: Schema.Types.ObjectId, ref: 'Messages' }],
+  _creator: { type: Schema.Types.ObjectId, ref: 'Users' },
+  messages: [{ type: Schema.Types.ObjectId, ref: 'Messages' }],
   name: String,
   members: String
 })
 
 var messageSchema = new Schema({
-  // _creator: { type: Schema.Types.ObjectId, ref: 'Users' },
-  // _group: { type: Schema.Types.ObjectId, ref: 'Groups' },
+  _creator: { type: Schema.Types.ObjectId, ref: 'Users' },
+  _group: { type: Schema.Types.ObjectId, ref: 'Groups' },
   message: String
 })
 
