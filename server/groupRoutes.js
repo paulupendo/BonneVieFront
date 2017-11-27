6d8f98b8267680d42e970864bd8b4f45773d86a4
candy_man <lovepaul46@outlook.com>
2017-11-27 15:41:48 +0300

    [feature]Implement token based auth.Add createGroup and addMemner routes

--- server/groupRoutes.js
+++ server/groupRoutes.js
@@ -6,14 +6,15 @@ var userController = require('./userController')
 
 router.post('/group', (req, res) => {
   var name = req.body.groupname
+  var token = req.body.token
   var group = new groupController.NewGroup(name) /**
    * new group object from constructor
    */
-  var response = userController.validator(group.name) /**
+  var response = userController.validator(group.name, token) /**
    * handles input validation
    */
   if (response === 'group input validation success') {
-    userController.createGroup(group.name, res) /**
+    userController.createGroup(group.name, token, res) /**
      * save group to DB
      */
   } else {
@@ -21,8 +22,15 @@ router.post('/group', (req, res) => {
   }
 })
 
-router.post('/group/<groupId>/message', (req, res) => {
+router.put('/group/:groupId/user', (req, res) => {
+  var email = req.body.email
+  var groupId = req.params.groupId
+  userController.addUser(email, groupId, res)
+})
+
+router.post('/group/:groupId/message', (req, res) => {
   var message = req.body.content
+  // var group_id = req.params.groupId
   var messageObj = new messageController.NewMessage(message)
   userController.createMesssage(messageObj.content, res)
 })
--- server/userController.js
+++ server/userController.js
@@ -1,5 +1,6 @@
 const R = require('ramda')
 var _db = require('../models/db_models')
+var authController = require('./authController')
 var groups = new _db.Groups()
 var messages = new _db.Messages()
 
@@ -10,29 +11,55 @@ const isEmptyString = R.pipe(
 )
 
 module.exports = {
-  validator: function (name) {
+  validator: function (name, token) {
     var response
     var emptyInput = R.any(isEmptyString, [name])
 
     if (emptyInput) {
       response = 'Ooops! Group name cannot be empty'
+    } else if (isEmptyString(token)) {
+      response = 'Ooops! Unauthorised'
     } else {
       response = 'group input validation success'
     }
     return response
   },
-  createGroup: function (name, res) {
+  createGroup: function (name, token, res) {
     groups.name = R.trim(name)
-    groups.save((err) => {
-      if (err) {
-        res.json({ msg: err.message })
-      } else {
-        res.json({ msg: 'Group successfuly created' })
-      }
-    })
+    var decodedToken = authController.verifyToken(token) /** decode token and get payload */
+    if (decodedToken.hasOwnProperty('id') === false) {
+      res.json({ msg: 'Authorisation error' })
+    } else if (decodedToken.hasOwnProperty('id')) {
+      _db.Users.findById(decodedToken.id, (err, user) => { /**
+       * locate user in DB with particular id
+       */
+        if (err) {
+          res.json({ msg: err })
+        } else if (String(user._id) === decodedToken.id) {
+          groups._creator = decodedToken.id
+          groups.save((err) => {
+            if (err) {
+              res.json({ msg: err.message })
+            } else {
+              res.json({ msg: 'Group successfuly created' })
+            }
+          })
+        } else {
+          res.json({ msg: 'Ooops! Unauthorised access!' })
+        }
+      })
+    }
   },
-  sendInvites: function (name, groupName) {
-    // Add users to group
+  addUser: function (email, groupid, res) {
+    var emptyData = R.any(isEmptyString, [email])
+    if (emptyData) {
+      res.json({ msg: 'Ooops! email cannot be empty' })
+    } else {
+      this.memberValidation(groupid, email, res) /**
+       * validates that group and user exists
+       * adds user to group's member list
+       */
+    }
   },
   createMesssage: function (_content, res) {
     messages.content = _content
@@ -44,6 +71,44 @@ module.exports = {
       }
     })
     // create new message
+  },
+  memberValidation: function (groupid, email, res) {
+    _db.Groups.findById(groupid, (err, group) => {
+      if (err) {
+        res.json({ msg: 'An error occured' })
+      } else if (group) {
+        _db.Users.find((err, users) => {
+          if (err) {
+            res.json({ msg: 'Ooops! An error occured' })
+          } else {
+            var userFound = users.find((user) => {
+              return user.email === email
+            })
+            if (userFound) {
+              var userExists = group.members.some((user) => {
+                return user.email === userFound.email
+              })
+              if (userExists) {
+                res.json({ msg: 'Ooops! This particular user already exists' })
+              } else {
+                group.members.push({ email: userFound.email }) /**
+                 * add new member to group
+                 * email serves as the identifier
+                 */
+                group.save((err) => {
+                  if (err) {
+                    res.json({ msg: 'Ooops! An error occured' })
+                  }
+                  res.json({ msg: userFound.username + ' was successfully added' })
+                }) /** update group info */
+              }
+            }
+          }
+        })
+      } else {
+        res.json({ msg: 'Ooops! group does not exist' })
+      }
+    })
   },
   deleteGroup: function () {
     // delete group
